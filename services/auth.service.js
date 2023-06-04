9a9db2bc7bbce9e6feaf722aecc3a1e18ad37633
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2023-06-05 01:40:38 +0530

    exception handler

--- services/auth.service.js
+++ services/auth.service.js
@@ -17,26 +17,35 @@ class AuthService {
   }
 
   async validateAndDecodeToken(token) {
-    const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log('admin', decode);
-    return decode;
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+      console.log('admin', decode);
+      return decode;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error(error.message);
+    }
   }
 
   async isAdmin(request, response, next) {
-    const { headers } = request;
-    const accessToken = headers?.authorization || headers?.token;
-    console.log(accessToken);
-    if (accessToken?.includes('Bearer')) {
-      const [_, token] = accessToken.split(' ');
-      const auth = new AuthService();
-      const user = await auth.validateAndDecodeToken(token);
-      if (user.isAdmin) {
-        next();
+    try {
+      const { headers } = request;
+      const accessToken = headers?.authorization || headers?.token;
+      console.log(accessToken);
+      if (accessToken?.includes('Bearer')) {
+        const [_, token] = accessToken.split(' ');
+        const auth = new AuthService();
+        const user = await auth.validateAndDecodeToken(token);
+        if (user.isAdmin) {
+          next();
+        } else {
+          response.status(401).send('You are not authorized to create an user');
+        }
       } else {
-        response.status(401).send('You are not authorized to create an user');
+        response.status(401).send('Not Authorized');
       }
-    } else {
-      response.status(401).send('Not Authorized');
+    } catch (error) {
+      return response.status(400).send('Token expired or invalid token');
     }
   }
 }
